9a5f527260b07a197264f9caae832bb53954b190
Ryan Cheung <rwcheung@uci.edu>
2022-10-21 10:07:20 -0700

    working serial communication

--- src/serial_test.ino
+++ src/serial_test.ino
@@ -6,31 +6,29 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define SERVOMIN  125 // this is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX  575 // this is the 'maximum' pulse length count (out of 4096)
 
-String str;
+unsigned int s;
 int angle;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.setTimeout(1);
+  pwm.begin();
+  pwm.setPWMFreq(60);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // pwm.setPWM(12, 0, angleToPulse(20));
   if (Serial.available() > 0) {
-    angle = Serial.parseInt();
+    s = Serial.read();
+    angle = (int)s;
     Serial.write(angle);
-    if (angle > 0) {
-        for(int i=0; i<angle; i+=5) {      
-            pwm.setPWM(12, 0, angleToPulse(i));
-            delay(5);
-        }
-      } else {
-        for(int i=0; i>angle; i -=5) {
-            pwm.setPWM(12, 0, angleToPulse(i));
-            delay(5);
-        }
-      } 
+    for(int i=0; i<angle; i+=5) {      
+        pwm.setPWM(12, i, angleToPulse(i));
+        delay(5);
+    }
+    Serial.flush();
   }
 }
 
--- src/serial_test.py
+++ src/serial_test.py
@@ -5,7 +5,7 @@ import time
 
 arduino = serial.Serial("COM3", baudrate=9600, timeout=1)
 while True:
-    arduino.write(struct.pack('!B', 25))
+    arduino.write(struct.pack('I', 100))
     time.sleep(2)
     from_arduino = arduino.readline()
     from_arduino = int.from_bytes(from_arduino, "little")
