2a91a53168623440ee2225be9b2024968f2ab679
Ryan Cheung <rwcheung@uci.edu>
2022-10-20 17:18:00 -0700

    cont.

--- src/serial_test.py
+++ src/serial_test.py
@@ -1,10 +1,12 @@
 import serial
 import struct
+import time
 
 
 arduino = serial.Serial("COM3", baudrate=9600, timeout=1)
 while True:
-    command = int(input("Servo position: "))
-    arduino.write(5)
+    arduino.write(struct.pack('!B', 25))
+    time.sleep(2)
     from_arduino = arduino.readline()
+    from_arduino = int.from_bytes(from_arduino, "little")
     print(from_arduino)
