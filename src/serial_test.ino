4174120bf2c003fb68187729226f2573ccba75e7
yasamin-m <x.yasaminm@gmail.com>
2022-10-26 18:19:47 -0700

    added serial comm for arduino

--- src/serial_test.ino
+++ src/serial_test.ino
@@ -8,10 +8,13 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 // pin# where servos are located
 uint8_t shoulder_servo = 12;
+uint8_t elbow_servo = 13;
 
 unsigned int s;
-int angle;
-int prev_angle;
+int prev_angle_shoulder = 0;
+int prev_angle_elbow = 0;
+
+String readString;
 
 void setup() {
   // put your setup code here, to run once:
@@ -21,6 +24,7 @@ void setup() {
   pwm.setPWMFreq(60);
   //initiate all servos to position 0
   pwm.setPWM(shoulder_servo, 0, angleToPulse(0));
+  pwm.setPWM(elbow_servo, 0, angleToPulse(0));
   delay(1000);
 }
 
@@ -34,24 +38,61 @@ int getDelay(int angle) {
   return delay;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  if (Serial.available() > 0) {
-    s = Serial.read();
-    angle = (int)s;
-    Serial.write(angle);    
-    if (angle > 0) {
-      pwm.setPWM(shoulder_servo, 0, angleToPulse(angle));
-      prev_angle = angle
-      delay(getDelay(abs(angle-prev_angle)));
-    }
-    Serial.flush();
-  }
-}
+// void loop() {
+//   // put your main code here, to run repeatedly:
+//   if (Serial.available() > 0) {
+//     s = Serial.read();
+//     angle = (int)s;
+//     Serial.write(angle);    
+//     if (angle > 0) {
+//       pwm.setPWM(shoulder_servo, 0, angleToPulse(angle));
+//       prev_angle = angle
+//       delay(getDelay(abs(angle-prev_angle)));
+//     }
+//     Serial.flush();
+//   }
+// }
 
 int angleToPulse(int ang){
    int pulse = map(ang,0, 180, SERVOMIN,SERVOMAX);// map angle of 0 to 180 to Servo min and Servo max 
   //  Serial.println("Angle: "); Serial.print(ang);
   //  Serial.print(" pulse: "); Serial.println(pulse);
    return pulse;
+}
+
+void loop() {
+
+  //expect single strings like 700a, or 1500c, or 2000d,
+  //or like 30c, or 90a, or 180d,
+  //or combined like 30c,180b,70a,120d,
+
+  if (Serial.available())  {
+    char c = Serial.read();  //gets one byte from serial buffer
+    if (c == ',') {
+      if (readString.length() >1) {
+        Serial.println(readString); //prints string to serial port out
+
+        int angle = readString.substring(1).toInt();  //convert readString into a number
+
+
+        Serial.print("writing Angle: ");
+        Serial.println(angle);
+        if(readString.indexOf('a') > 0){ 
+          pwm.setPWM(shoulder_servo, 0, angleToPulse(angle));
+          delay(getDelay(abs(angle-prev_angle_shoulder));
+          prev_angle_shoulder = angle;
+        }
+        if(readString.indexOf('b') >0) {
+          pwm.setPWM(13, 0, angleToPulse(angle));
+          delay(getDelay(abs(angle-prev_angle_elbow));
+          prev_angle_elbow = angle;
+        }
+
+        readString=""; //clears variable for new input
+      }
+    }  
+    else {     
+      readString += c; //makes the string readString
+    }
+  }
 }
